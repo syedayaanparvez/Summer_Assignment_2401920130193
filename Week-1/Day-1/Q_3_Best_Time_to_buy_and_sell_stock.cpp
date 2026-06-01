6ce0945198f37b59d25c19daf93e9a63b46b1334
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-06-01 20:57:14 +0530

    Update Q_3_Best_Time_to_buy_and_sell_stock.cpp

--- Week-1/Day-1/Q_3_Best_Time_to_buy_and_sell_stock.cpp
+++ Week-1/Day-1/Q_3_Best_Time_to_buy_and_sell_stock.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_prices=prices[0];
+        int max_profit=0;
 
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]<min_prices){
+                min_prices=prices[i];
+            }
+            else{
+                max_profit=max(max_profit,prices[i]-min_prices);
+            }
+        }
+    return max_profit;;
+
+
+    }
+};
