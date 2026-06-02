b17bc7261f6c5b135bf98f54797182993ec23474
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-06-02 23:55:33 +0530

    Update Q_3_Max_Avg_subarray.cpp

--- Week-1/Day-2/Q_3_Max_Avg_subarray.cpp
+++ Week-1/Day-2/Q_3_Max_Avg_subarray.cpp
@@ -1,1 +1,24 @@
+ class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
 
+        long long sum = 0;
+
+         
+        for(int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        long long maxSum = sum;
+
+        
+        for(int i = k; i < nums.size(); i++) {
+
+            sum = sum - nums[i-k] + nums[i];
+
+            maxSum = max(maxSum, sum);
+        }
+
+        return (double)maxSum / k;
+    }
+};
