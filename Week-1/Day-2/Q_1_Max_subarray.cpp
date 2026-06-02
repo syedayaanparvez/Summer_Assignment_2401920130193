436d4a17946e5b151877f80f9f087f397f78003a
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-06-02 23:26:58 +0530

    Update Q_1_Max_subarray.cpp

--- Week-1/Day-2/Q_1_Max_subarray.cpp
+++ Week-1/Day-2/Q_1_Max_subarray.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
 
+        int sum = 0;
+        int ans = nums[0];
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            sum += nums[i];
+
+            ans = max(ans, sum);
+
+            if(sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return ans;
+    }
+};
