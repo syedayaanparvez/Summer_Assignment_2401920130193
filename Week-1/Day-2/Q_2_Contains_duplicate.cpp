93847c136d819bf1f7f2005b126ac82064435864
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-06-02 23:44:07 +0530

    Update Q_2_Contains_duplicate.cpp

--- Week-1/Day-2/Q_2_Contains_duplicate.cpp
+++ Week-1/Day-2/Q_2_Contains_duplicate.cpp
@@ -1,1 +1,17 @@
+#include <unordered_set>
 
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+
+        for(int num : nums) {
+            if(s.find(num) != s.end()) {
+                return true;
+            }
+            s.insert(num);
+        }
+
+        return false;
+    }
+};
