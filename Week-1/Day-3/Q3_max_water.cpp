1f5c0fa8b3151db082627a2ad1cc37e37d406323
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-06-03 22:43:58 +0530

    Update Q3_max_water.cpp

--- Week-1/Day-3/Q3_max_water.cpp
+++ Week-1/Day-3/Q3_max_water.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int max_water=0;
+        int left=0;
+        int right=height.size()-1;
+        while(left<right){
+            int width=right-left;
+            int current_height=min(height[left],height[right]);
+            int current_water=width*current_height;
+            max_water=max(max_water,current_water);
 
+            if(height[left]<height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return max_water;
+
+    }
+};
