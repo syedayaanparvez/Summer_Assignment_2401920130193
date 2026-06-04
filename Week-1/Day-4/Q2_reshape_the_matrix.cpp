789688559a4f01502c039b60e1835fdf0b488ec5
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-06-04 13:30:36 +0530

    Update Q2_reshape_the_matrix.cpp

--- Week-1/Day-4/Q2_reshape_the_matrix.cpp
+++ Week-1/Day-4/Q2_reshape_the_matrix.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int rows=mat.size();
+        int cols=mat[0].size();
+        if(rows*cols!=r*c){
+            return mat;
+        }
+        vector<vector<int>> ans(r,vector<int>(c));
+        int k=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                ans[k/c][k%c]=mat[i][j];
+                k++;
+            }
+        }
+        return ans;     
+    }
+};
