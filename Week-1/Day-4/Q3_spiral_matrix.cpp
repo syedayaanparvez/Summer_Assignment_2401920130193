8df02fcdbc61601fca8b76c009f99024636d0c57
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-06-04 22:01:33 +0530

    Update Q3_spiral_matrix.cpp

--- Week-1/Day-4/Q3_spiral_matrix.cpp
+++ Week-1/Day-4/Q3_spiral_matrix.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>ans;
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        int left=0;
+        int top=0;
+        int right=cols-1;
+        int bottom=rows-1;
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++)
+            ans.push_back(matrix[top][i]);
+            top++;    
+    
+            for(int j=top;j<=bottom;j++)
+            ans.push_back(matrix[j][right]);
+            right--;
 
+            if(top<=bottom){
+                for(int k=right;k>=left;k--)
+                ans.push_back(matrix[bottom][k]);
+                bottom--;
+            }
+             
+            if(left<=right){
+                for(int l=bottom;l>=top;l--)
+                ans.push_back(matrix[l][left]);
+                left++;
+            }
+             
+        }
+        return ans;
+         
+    }
+};
