b05599d1956e9d30fd3838e2d89d726ee1e39654
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-06-04 12:49:04 +0530

    Update Q1_diagonal_sum_mat.cpp

--- Week-1/Day-4/Q1_diagonal_sum_mat.cpp
+++ Week-1/Day-4/Q1_diagonal_sum_mat.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int suml=0;
+        int sumr=0;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat.size();j++){
+                if(i==j){
+                    suml=suml+mat[i][j];
+                }
+                else if(i+j==mat.size()-1){
+                    sumr+=mat[i][j];
+                }
+            }
+        }
+    return (suml+sumr);    
+    }
+};
