97da8501324b5535e988231ba6ca2cd485fde649
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-06-05 19:50:18 +0530

    Update Q1_valid_palindrome.cpp

--- Week-1/Day-5/Q1_valid_palindrome.cpp
+++ Week-1/Day-5/Q1_valid_palindrome.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans="";
+        for(char ch:s){
+            if(isalnum(ch)){
+                ans+=tolower(ch);
+            }
+        }
+        int start=0;
+        int end=ans.size()-1;
+        while(start<end){
+            if(ans[start]!=ans[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
