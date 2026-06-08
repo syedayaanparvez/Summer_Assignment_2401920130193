6e881e80a1c9991a548a1305fc184ddf699bf758
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-06-08 23:15:28 +0530

    Update Q3_ransome_note.cpp

--- Week-2/Day-1/Q3_ransome_note.cpp
+++ Week-2/Day-1/Q3_ransome_note.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> m1;
+        unordered_map<char,int> m2;
 
+        for(char c1:ransomNote){
+            m1[c1]++;
+        }
+        for(char c2:magazine){
+            m2[c2]++;
+        }
+
+        for(auto it : m1){
+            if(m2[it.first] < it.second){
+                return false;
+            }
+        }
+        return true;
+    }
+};
