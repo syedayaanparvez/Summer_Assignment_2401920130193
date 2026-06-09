f51b7167d32ae3de9750800dc2d6ee273a4e4cfa
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-06-09 23:23:47 +0530

    Update Q1_longest_substring_without_repeting_char.cpp

--- Week-2/Day-2/Q1_longest_substring_without_repeting_char.cpp
+++ Week-2/Day-2/Q1_longest_substring_without_repeting_char.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.size();
+        int ans=1;
+        int l=0;
+        vector<int> freq(256,0);
+        if(n==0)
+        return 0;
 
+        for(int r=0;r<n;r++){
+            freq[s[r]]++;
+            while(freq[s[r]]>1){
+                freq[s[l]]--;
+                l++;
+            }
+            ans=max(ans,r-l+1);
+        }
+        return ans;
+    }
+};
