437673adcabdd9f8b6ba9294c7d59a0fd020ca1e
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-07-08 19:49:04 +0530

    Update Q1_merge_two_sorted_lists.cpp

--- Week-3/Day-2/Q1_merge_two_sorted_lists.cpp
+++ Week-3/Day-2/Q1_merge_two_sorted_lists.cpp
@@ -1,1 +1,44 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL){
+            return list2;
+        }
 
+        if(list2==NULL){
+            return list1;
+        }
+        ListNode *head=new ListNode(0);
+        ListNode *tail=head;
+        while(list1 && list2)
+        {
+            if(list1->val<list2->val)
+            {
+                tail->next=list1;
+                list1=list1->next;
+                tail=tail->next;
+                tail->next=NULL;
+            }
+            else
+            {
+                tail->next=list2;
+                list2=list2->next;
+                tail=tail->next;
+                tail->next=NULL;
+            }
+        
+        }
+        if(list1)
+        {
+            tail->next=list1;
+        }
+        else
+        {
+            tail->next=list2;
+        }
+        tail=head;
+        head=head->next;
+        delete tail;
+        return head;
+    }
+};
