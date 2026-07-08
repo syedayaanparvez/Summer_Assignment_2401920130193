99d9a7db65cabf7d8017e9cc6aa6b6ea036d92d4
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-07-08 19:44:24 +0530

    Update Q2_Reverse_linked_list.cpp

--- Week-3/Day-1/Q2_Reverse_linked_list.cpp
+++ Week-3/Day-1/Q2_Reverse_linked_list.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *curr=head, *prev=NULL, *fut=NULL;
+        while(curr)
+        {
+            fut=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=fut;
+        }
+        head=prev;
+        return head;
+    }
+};
