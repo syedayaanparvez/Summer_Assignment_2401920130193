72468ae296ebf7824f9be0acccc07d039026bb07
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-07-08 19:51:21 +0530

    Update Q3_palindrome_linked_lists.cpp

--- Week-3/Day-2/Q3_palindrome_linked_lists.cpp
+++ Week-3/Day-2/Q3_palindrome_linked_lists.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> ans;
+        ListNode *temp=head;
+        while(temp)
+        {
+            ans.push_back(temp->val);
+            temp=temp->next;
+        }
+        int first=0;
+        int second=ans.size()-1;
+        while(first<second)
+        {
+            if(ans[first]!=ans[second])
+            {
+                return false;
+            }
+             
+            first++;
+            second--;
+        }
+        return true;;
+    }
+};
