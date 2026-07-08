24df77779040fcc9a66c13275605dc18467216d7
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-07-08 19:50:31 +0530

    Update Q2_remove_nth_node.cpp

--- Week-3/Day-2/Q2_remove_nth_node.cpp
+++ Week-3/Day-2/Q2_remove_nth_node.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int count=0;
+        ListNode *temp=head;
+        while(temp!=NULL)
+        {
+            count++;
+            temp=temp->next;
+        }
+        count=count-n;
+        if(count==0)
+        {
+            ListNode *temp=head;
+            head=head->next;
+            delete temp;
+            return head;
+        }
+        ListNode *curr=head,*prev=NULL;
 
+        while(count--)
+        {
+            prev=curr;
+            curr=curr->next;
+        }
+        prev->next=curr->next;
+        delete curr;
+        return head;
+    }
+};
