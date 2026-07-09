4cffc5db095dd504268851f1a189579b83080426
Syed Mohammad Ayaan Parvez <ayansyd469@gmail.com>
2026-07-09 11:31:41 +0530

    Update Q1-valid_parentheses.cpp

--- Week-3/Day-3/Q1-valid_parentheses.cpp
+++ Week-3/Day-3/Q1-valid_parentheses.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> st;
+        for(int i=0;i<str.size();i++){
+            if(str[i]=='(' || str[i]=='{' || str[i]=='['){
+                st.push(str[i]);
+            }
+            else{// closing
+                if(st.size()==0){
+                    return false;
+                }
+                else if((st.top()=='(' && str[i]==')')||(st.top()=='{' && str[i]=='}')||(st.top()=='[' && str[i]==']')){
+                    st.pop();
+                }else{// no match
+                    return false;
+                }
 
+            }
+        }
+        return st.size()==0;
+    }
+};
